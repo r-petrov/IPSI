f08d752b34def80420a989696dfa10355812f57f
r-petrov <r_petroff@yahoo.com>
2018-12-29 23:19:45 +0200

    Change Bootstrap theme
    Change default project Bootstrap theme to Spacelab theme

--- src/Tests/Sandbox/Program.cs
+++ src/Tests/Sandbox/Program.cs
@@ -21,6 +21,7 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
+    using System.Linq;
 
     public static class Program
     {
